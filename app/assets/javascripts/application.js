68b6db8308ec9dab1415272e33610cb10a3cf2c3
Seth Boyles <sethboyles@gmail.com>
2014-01-29 22:39:30 -0800

    initial setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,14 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require litbase
+
+// for more details see: http://emberjs.com/guides/application/
+Litbase = Ember.Application.create();
+
 //= require_tree .
